770ddbac492d2d49e77723abd731ca80d4fd69e4
Ankit <ankit.bharat.tanna@accenture.com>
2015-12-01 17:23:26 +0530

    AM: Search Screen

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+    .when('/search', {
+        templateUrl: 'views/search.html',
+        controller: 'SearchCtrl',
+        controllerAs: 'search'
+      })
       .otherwise({
         redirectTo: '/'
       });
