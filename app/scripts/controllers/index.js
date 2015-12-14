4fa1ae82cc0c1c30f0557af42e67a2f056b3717c
Parth19 <parth.thakkar@accenture.com>
2015-12-14 16:13:49 +0530

    User Profile Controller, Breadcrumb
    User Profile & Breadcrumb formatting

---
+++ app/scripts/controllers/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appModernizationApp.controller:IndexCtrl
+ * @description
+ * # IndexCtrl
+ * Controller of the appModernizationApp
+ */
+angular.module('appModernizationApp')
+  .controller('IndexCtrl', ['$scope','$http','HRS','$location','breadcrumbs',function ($scope,$http,HRS,$location,breadcrumbs) {
+    
+    $scope.uesrName = "John Doe";
+    $scope.breadcrumbs = breadcrumbs;
+    
+  }]);
