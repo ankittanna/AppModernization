2bec7371c08b6b6e5f708d855443a8ef5519b13e
Vinod Khandelwal <vinod.khandelwal@gmail.com>
2015-12-17 08:02:47 +0000

    Base URL Change
    Base URL Change

--- app/scripts/factory/appModernizationFactory.js
+++ app/scripts/factory/appModernizationFactory.js
@@ -6,7 +6,7 @@ hotelReservationServices.$inject = ['$http'];
 
 function hotelReservationServices($http)
 {   
-    var baseUrl = 'http://10.168.11.33:8080/authrestserv-war';
+    var baseUrl = 'http://172.31.28.248:9001/am/v1';
     var reservedData = "";
     // Object Map of functions
     return {
