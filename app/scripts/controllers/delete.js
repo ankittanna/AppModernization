eaa651abd9164d008cf435cdfd3985ce40bb0344
Vinod Khandelwal <vinod.khandelwal@gmail.com>
2015-12-24 15:17:43 +0530

    delete file change

--- app/scripts/controllers/delete.js
+++ app/scripts/controllers/delete.js
@@ -8,42 +8,22 @@
  * Controller of the appModernizationApp
  */
 angular.module('appModernizationApp')
-    .controller('DeleteCtrl', ['$scope', '$http', 'HRS', '$location', '$routeParams', 'breadcrumbs', function($scope, $http, HRS, $location, $routeParams, breadcrumbs) {
+    .controller('DeleteCtrl', ['$scope', '$http', 'HRS', '$location', '$routeParams', 'breadcrumbs', 'UtilitiesService',function($scope, $http, HRS, $location, $routeParams, breadcrumbs, UtilitiesService) {
 
         $scope.breadcrumbs = breadcrumbs;
 
+        $scope.reservationDetails = [];
         var reservedData = [];
         $scope.reservationId = $routeParams.param1;
 
         HRS.getRegisteredData($scope.reservationId).then(function(data) {
             reservedData = data;
             console.log(JSON.stringify(reservedData));
+            reservedData = data;
+            reservedData.arrivalDate = UtilitiesService.convertToFormat(reservedData.arrivalDate);
+            reservedData.departureDate = UtilitiesService.convertToFormat(reservedData.departureDate);
+            $scope.reservationDetails = reservedData;
 
-            $scope.reservationId = reservedData.reservationId;
-
-            var arrival = reservedData.arrivalDate.toString();
-            var departure = reservedData.departureDate.toString();
-            $scope.arrivalDate = arrival.slice(6, 8) + "/" + arrival.slice(4, 6) + "/" + arrival.slice(0, 4);
-            $scope.departureDate = departure.slice(6, 8) + "/" + departure.slice(4, 6) + "/" + departure.slice(0, 4);
-            $scope.roomType = reservedData.room.roomType;
-            $scope.roomNumber = reservedData.room.roomNo;
-            $scope.roomDesc = reservedData.room.roomDescription;
-            $scope.roomRate = reservedData.room.rate;
-            $scope.rateCode = reservedData.room.rateCode;
-            $scope.smokingFlag = reservedData.room.smokeFlag;
-            $scope.lateArrivalFlag = reservedData.lateArrivalFlag;
-            $scope.firstName = reservedData.customer.firstName;
-            $scope.lastName = reservedData.customer.lastName;
-            $scope.middleName = reservedData.customer.middleName;
-            $scope.addressLine1 = reservedData.customer.addressLine1;
-            $scope.addressLine2 = reservedData.customer.addressLine2;
-            $scope.addressLine3 = reservedData.customer.addressLine3;
-            $scope.companyName = reservedData.customer.companyName;
-            $scope.phoneNumber = reservedData.customer.phoneNumber;
-            $scope.cardType = reservedData.cardType;
-            $scope.cardNumber = reservedData.cardNumber;
-            $scope.expiryDate = reservedData.expiryDate;
-            $scope.comments = reservedData.comments1;
         }).catch(function(response) {
             // TODO: Is this really required?
             // angular.element('#roomTable').css('display', 'none'); 
@@ -53,10 +33,8 @@ angular.module('appModernizationApp')
         this.registrationErrorMessage = '';
 
         this.deleteReservation = function() {
-            console.log("Inside Delte reservation");
             HRS.cancleReservation($scope.reservationId).then(function(data) {
-                this.registrationErrorMessage = '';
-                
+                this.registrationErrorMessage = '';                
                 var reservationId = data.reservationId;
                 console.log("Detail Data  " + JSON.stringify(data));
                 $location.path('/search');     
