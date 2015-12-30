f06a09927c13742308eeba1eb199d42662dab06d
Parth19 <parth.thakkar@accenture.com>
2015-12-30 14:23:24 +0530

    Custom Directive
    custom direcrtive

---
+++ app/scripts/Directive/roomdetails.js
@@ -0,0 +1,13 @@
+angular.module('appModernizationApp')
+    .directive('roomDetails', function () {
+    return {
+        restrict: 'EA', //E = element, A = attribute, C = class, M = comment         
+       // scope: {
+            //@ reads the attribute value, = provides two-way binding, & works with functions
+         //   title: '@'         },
+        //template: 'Name: {{customer.name}}<br /> Street: {{customer.street}}',
+        templateUrl: '../../template/roomdetails.html',
+        controller: 'ViewCtrl' //Embed a custom controller in the directive
+        //link: function ($scope, element, attrs) { } //DOM manipulation
+    }
+});
