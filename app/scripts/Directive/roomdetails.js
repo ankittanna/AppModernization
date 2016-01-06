18184f058fcc8681d48bc4a9bd74f2a60c7ec798
Vinod Khandelwal <vinod.khandelwal@gmail.com>
2016-01-06 19:24:32 +0530

    UpperCase issue
    UpperCase issue

--- app/scripts/Directive/roomdetails.js
+++ app/scripts/Directive/roomdetails.js
@@ -7,8 +7,8 @@ angular.module('appModernizationApp')
             //@ reads the attribute value, = provides two-way binding, & works with functions
          //   title: '@'         },
         //template: 'Name: {{customer.name}}<br /> Street: {{customer.street}}',
-        templateUrl: '../../template/roomdetails.html',
-        controller: 'ViewCtrl' //Embed a custom controller in the directive
+        templateUrl: '../../template/roomdetails.html'
+        //controller: 'ViewCtrl' //Embed a custom controller in the directive
         //link: function ($scope, element, attrs) { } //DOM manipulation
     };
 });
