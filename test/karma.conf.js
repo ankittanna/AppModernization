b4a4ed71b84de666cd8733a459093957da94af1c
Ankit <ankit.bharat.tanna@accenture.com>
2016-01-07 15:01:34 +0530

    Karma: Test Config
    Karma: Test Config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: [config.LOG_INFO, config.LOG_DISABLE, config.LOG_ERROR, config.LOG_WARN, config.LOG_DEBUG],
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
